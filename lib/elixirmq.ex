6387c1f01888cb457eb1bac43fbfb53f73219670
Mikołaj Leonhardt <mikolaj.leonhardt@gmail.com>
2025-05-26 23:31:04 +0200

    mix init

---
+++ lib/elixirmq.ex
@@ -0,0 +1,19 @@
+defmodule Elixirmq do
+  @moduledoc """
+  Documentation for `Elixirmq`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Elixirmq.hello()
+      :world
+
+  """
+  def hello do
+    IO.puts "Hello World"
+  end
+
+end
